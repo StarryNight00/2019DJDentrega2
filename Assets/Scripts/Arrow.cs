91705658fe862466737ffa31f6aa95905bc00404
StarryNight00 <cisa.matias@gmail.com>
2019-05-27 02:49:14 +0100

    projeto preregrine

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    //OBJECT ARROW. STATES MOVEMENT AND CONTROLS COLIDER
+    [Header ("Arrow")]
+
+    [SerializeField] Collider2D collider;
+
+    private float       speed = 400;
+    private float       horizontalAmplitude = 0;
+    private float       horizontalFrequency = 0;
+    private float       timer;
+    private float       angle;
+
+    public Vector3      moveVector;
+    private Vector3     startPos;
+    private Vector3     hVector;
+    public Transform    hitPos;
+
+    void Start()
+    {
+        collider = GetComponent<Collider2D>();
+
+        hVector.Normalize();
+
+        timer = 0.0f;
+        angle = 0.0f;
+
+        hVector = new Vector3(moveVector.y, moveVector.x, moveVector.z);
+
+        startPos = transform.position;
+        
+    }
+
+    private void FixedUpdate()
+    {
+        hVector.y = startPos.y;
+        timer += Time.fixedDeltaTime;
+        angle += horizontalFrequency * Time.fixedDeltaTime;
+
+        float s = horizontalAmplitude * Mathf.Sin(angle * Mathf.Deg2Rad);
+
+        transform.position = startPos + moveVector * speed * timer + hVector * s;
+    }
+
+    private void Update()
+    {
+        ContactFilter2D contactFilter = new ContactFilter2D();
+        contactFilter.ClearLayerMask();
+        contactFilter.SetLayerMask(LayerMask.GetMask("Enemy"));
+
+        Collider2D[] results = new Collider2D[6];
+
+        int nCollisions = Physics2D.OverlapCollider(collider, contactFilter, results);
+
+        if (nCollisions > 0)
+        {
+            for (int i = 0; i < nCollisions; i++)
+            {
+                collider = results[i];
+
+                Enemy enemy = collider.GetComponent<Enemy>();
+                if (enemy)
+                {
+                    enemy.GetNumb();
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(hitPos.position, 2.0f);
+    }
+
+}
---
+++ Assets/Scripts/ShotControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class ShotControl : MonoBehaviour
+{
+    //CONTROLS THE SHOT ABILITY. INSTATIATES THE ARROW
+
+    Player              player;
+    PlayerController    playerCtrl;
+
+    private bool        arrowShot;
+
+    public float        cooldownTime = 2.5f;
+    public GameObject   projectile;
+
+    float cooldown;
+
+    void Start()
+    {
+        cooldown = 0.0f;
+    }
+
+    void FixedUpdate()
+    { 
+        cooldown -= Time.fixedDeltaTime;
+
+        if (cooldown < 0.0f)
+        {
+            arrowShot = Input.GetButton("Fire1");
+
+            if (arrowShot)
+            {
+                Shot();
+            }
+        }
+    }
+
+    void Shot()
+    {
+        Quaternion rotation = transform.rotation;
+        Instantiate(projectile, transform.position, rotation);
+        cooldown = cooldownTime;
+    }
+
+}
